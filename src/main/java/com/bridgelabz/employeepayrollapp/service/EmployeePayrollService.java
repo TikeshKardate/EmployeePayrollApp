39d56575d5011cf37b54380f1be691a8cb13976d
TikeshKardate <tikesh.kardate@gmail.com>
2025-02-27 15:37:41 +0530

    [Tikesh] Add. Employee Data stored in list in Service Layer added

--- src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
@@ -1,49 +1,52 @@
 package com.bridgelabz.employeepayrollapp.service;
 
 import com.bridgelabz.employeepayrollapp.model.Employee;
-import com.bridgelabz.employeepayrollapp.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 public class EmployeePayrollService {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private final List<Employee> employeeList = new ArrayList<>();
+    private Long employeeIdCounter = 1L;
 
-    // Create Employee
+    // Create Employee in the list format
     public Employee createEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+        employee.setId(employeeIdCounter++); //Manually set unique ID
+        employeeList.add(employee);
+        return employee;
     }
 
     // Get All Employees
     public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
+        return employeeList;
     }
 
     // Get Employee by ID
     public Optional<Employee> getEmployeeById(Long id) {
-        return employeeRepository.findById(id);
+        return employeeList.stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst();
     }
 
     // Update Employee
     public Optional<Employee> updateEmployee(Long id, Employee updatedEmployee) {
-        return employeeRepository.findById(id).map(employee -> {
+        return employeeList.stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst()
+                .map(employee -> {
             employee.setName(updatedEmployee.getName());
             employee.setSalary(updatedEmployee.getSalary());
-            return employeeRepository.save(employee);
+            return employee;
         });
     }
 
     // Delete Employee
     public boolean deleteEmployee(Long id) {
-        if (employeeRepository.existsById(id)) {
-            employeeRepository.deleteById(id);
-            return true;
-        }
-        return false;
+    return employeeList.removeIf(employee -> employee.getId().equals(id));
     }
 }
